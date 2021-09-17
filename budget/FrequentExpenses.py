60ce257abf6ed3cdb8c54d0781928b92332b91c9
Megan <smith.megan.design@gmail.com>
2021-09-17 11:39:53 -0600

    initial test

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,7 @@
+import Expense
+expenses=Expense.Expenses()
+expenses.read_expenses("data/spending_data.csv")
+
+spending_categories=[]
+for expense in expenses.list:
+  spending_categories.append(expenses.category)
