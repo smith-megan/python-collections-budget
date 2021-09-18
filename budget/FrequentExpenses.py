5bb4fa06d95d45335b7b590943eb4279e3d6bcca
Megan <smith.megan.design@gmail.com>
2021-09-18 07:55:46 -0600

    step two progress finished top 5 function

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,7 +1,17 @@
-import Expense
+from . import Expense
+import collections
+
 expenses=Expense.Expenses()
-expenses.read_expenses("data/spending_data.csv")
+# print(expenses)
+expenses.read_expenses('data/spending_data.csv')
+
 
 spending_categories=[]
+# print(expenses.category)
+# For loop over expense list, to append spending list with each category
 for expense in expenses.list:
-  spending_categories.append(expenses.category)
+    spending_categories.append(expense.category)
+
+spending_counter=collections.Counter(spending_categories)
+top5=spending_counter.most_common(5)
+print(top5)
