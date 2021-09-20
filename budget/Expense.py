f7fcfc6afded259ab78038c788794492fe010b16
Megan <smith.megan.design@gmail.com>
2021-09-20 12:18:31 -0600

    pie chart completed

--- budget/Expense.py
+++ budget/Expense.py
@@ -43,10 +43,12 @@ class Expenses():
 
 
     def categorize_set_comprehension(self):
-        necessary_expenses={}
-        {x for x in self.list}
-        if (x.category == 'Phone' or x.category == 'Auto and Gas' or x.category== 'Classes' or x.category == 'Utilities' or x.category=='Mortgage'):
-            pass
-        food_expenses=[]
-        unnecessary_expenses=set(self.list)
-        return necessary_expenses, food_expenses, unnecessary_expenses
+        necessary_expenses={x for x in self.list
+        if (x.category == 'Phone' or x.category == 'Auto and Gas' or x.category== 'Classes' or x.category == 'Utilities' or x.category=='Mortgage')
+        }
+        food_expenses={x for x in self.list
+        if x.category=='Groceries' or x.category=='Eating Out'}
+
+        unnecessary_expenses=set(self.list)-necessary_expenses-food_expenses
+        
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
